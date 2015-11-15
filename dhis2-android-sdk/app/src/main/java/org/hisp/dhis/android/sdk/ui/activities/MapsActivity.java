b30d7f2d9e1c3e469c692625dd50047949f67ee1
idamfro <idamfro@ulrik.uio.no>
2015-11-15 19:47:41 +0100

    commit before checkout

--- dhis2-android-sdk/app/src/main/java/org/hisp/dhis/android/sdk/ui/activities/MapsActivity.java
+++ dhis2-android-sdk/app/src/main/java/org/hisp/dhis/android/sdk/ui/activities/MapsActivity.java
@@ -9,12 +9,12 @@ import android.widget.Button;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 import org.hisp.dhis.android.sdk.R;
 
 public class MapsActivity extends FragmentActivity {
@@ -24,6 +24,7 @@ public class MapsActivity extends FragmentActivity {
     Marker clickedPosition;
     Button setLocationButton;
     public final int PICK_COORDINATE_REQUEST = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,10 +63,19 @@ public class MapsActivity extends FragmentActivity {
         //   android.os.Debug.stopMethodTracing();
     }
     /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
      * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
      */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
@@ -79,8 +89,17 @@ public class MapsActivity extends FragmentActivity {
             }
         }
     }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
     private void setUpMap() {
-        LatLng gjovik = new LatLng(60.1, 23.1);
+        //  Location location = GpsController.getLocation();
+        //LatLng gjovik = new LatLng(location.getLatitude(),location.getLongitude());
+        LatLng gjovik = new LatLng(60.1,23.1);
         mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         mMap.addMarker(new MarkerOptions().position(gjovik).title("YourPosition").alpha(0.7f));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(gjovik));
@@ -88,15 +107,15 @@ public class MapsActivity extends FragmentActivity {
     }
 
 
-    private GoogleMapOptions getOptions(){
-        GoogleMapOptions options = new GoogleMapOptions();
-        options.mapType(GoogleMap.MAP_TYPE_TERRAIN)
-                .compassEnabled(true)
-                .rotateGesturesEnabled(true)
-                .tiltGesturesEnabled(true);
-        return options;
-    }
-
+/**    private GoogleMapOptions getOptions(){
+ GoogleMapOptions options = new GoogleMapOptions();
+ options.mapType(GoogleMap.MAP_TYPE_TERRAIN)
+ .compassEnabled(true)
+ .rotateGesturesEnabled(true)
+ .tiltGesturesEnabled(true);
+ return options;
+ }
+ **/
     /**
      * Listener for clicked
      */
@@ -123,37 +142,5 @@ public class MapsActivity extends FragmentActivity {
              .commit();**/
         }
     }
+
 }
- /**extends FragmentActivity {
-
-
-
-
-
-
-    **/
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
-
-  /**
-
-
-
-
-/**
- **/
-
-
